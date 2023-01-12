59e0e9fdf86ea6a494f530b6e4c7ad9ffc06eb6c
Ariful Islam <117193200+ariftutul@users.noreply.github.com>
2023-01-12 15:25:48 +0600

    file upload

---
+++ calculation.js
@@ -0,0 +1,11 @@
+let a = 20;
+let b = 10;
+let sum = a + b;
+let sub = a - b;
+let mul = a * b;
+let div = a / b;
+
+console.log("sum = " + sum);
+console.log('sub = ' + sub);
+console.log(` mul= ` + mul);
+console.log('div = ' + div);
